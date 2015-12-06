f03ed727342a28698f98a7d2a2bc1d158b8991cb
jibpy <weerapan083@gmail.com>
2015-12-06 14:50:53 +0700

    เพิ่มเสียงหน้าแรก

--- app/src/main/java/rtc/phoampai/lalita/mytraffic/SplashScreen.java
+++ app/src/main/java/rtc/phoampai/lalita/mytraffic/SplashScreen.java
@@ -1,5 +1,8 @@
 package rtc.phoampai.lalita.mytraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,22 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-    }
-}
+
+        Handler objHandler = new Handler();
+        objHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent objIntent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(objIntent);
+                finish();
+            }
+        },8000);
+
+        MediaPlayer introMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.intro_start_horse);
+        introMediaPlayer.start();
+
+
+
+    }   //Main Method
+
+}   //Main Class
